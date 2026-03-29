2e72e9c02587952830872efdded529fd936d4115
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-30 03:42:20 +0600

    stats section done with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import NavBar from './Components/NavBar/NavBar'
+import Stats from './Components/Stats/Stats'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <div className='max-w-480'>
      <NavBar />
      <Banner />
+     <Stats />
     </div>
   )
 }
