1612645e396306aef7234bb33ad04a51d318db07
tfshorifulislam <tfshorifulislam@gmail.com>
2026-03-30 12:34:24 +0600

    stats section text css update

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -6,22 +6,22 @@ const Stats = () => {
             <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA]'>
                 <div className='flex justify-between  gap-3 py-7 lg:py-14 px-5 lg:px-48 text-white text-center'>
                     <div className=''>
-                        <h2 className='font-semibold md:font-extrabold text-2xl lg:text-4xl mb-5'>50K+</h2>
-                        <p className='font-medium text-xl'>Active Users</p>
+                        <h2 className='font-semibold md:font-extrabold text-3xl lg:text-4xl md:mb-5 mb-2'>50K+</h2>
+                        <p className=''>Active Users</p>
                     </div>
                     
                      <div className='border-r-2 border-white opacity-30'></div>
 
                     <div>
-                        <h1 className='font-semibold md:font-extrabold text-2xl lg:text-4xl mb-5'>200+</h1>
-                        <p className='font-medium text-xl'>Premium Tools</p>
+                        <h1 className='font-semibold md:font-extrabold text-3xl lg:text-4xl md:mb-5 mb-2'>200+</h1>
+                        <p className=''>Premium Tools</p>
                     </div>
 
                     <div className='border-r-2 border-white opacity-30'></div>
 
                     <div>
-                        <h2 className='font-extrabold text-2xl lg:text-4xl mb-5'>4.9</h2>
-                        <h2 className='font-medium text-xl'>Rating</h2>
+                        <h2 className='font-extrabold text-3xl lg:text-4xl md:mb-5 mb-2'>4.9</h2>
+                        <h2 className=''>Rating</h2>
                     </div>
                 </div>
             </div>
